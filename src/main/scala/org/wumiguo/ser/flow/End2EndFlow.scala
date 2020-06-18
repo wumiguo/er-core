4d03b8f537e85eaf5afe329231ccf2fb48cc8a92
levinliu <levinmhliu@gmail.com>
2020-06-18 11:05:26 +0800

    ADD flow

---
+++ src/main/scala/org/wumiguo/ser/flow/End2EndFlow.scala
@@ -0,0 +1,10 @@
+package org.wumiguo.ser.flow
+
+/**
+ * @author levinliu
+ *         Created on 2020/6/18
+ *         (Change file header on Settings -> Editor -> File and Code Templates)
+ */
+object End2EndFlow {
+
+}
