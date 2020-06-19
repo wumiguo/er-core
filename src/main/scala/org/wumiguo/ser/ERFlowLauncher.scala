585bf7df7c987a537808cccf548e5e81d8938fe6
levinliu <levinmhliu@gmail.com>
2020-06-19 11:15:46 +0800

    ADD log4j

--- src/main/scala/org/wumiguo/ser/ERFlowLauncher.scala
+++ src/main/scala/org/wumiguo/ser/ERFlowLauncher.scala
@@ -1,5 +1,6 @@
 package org.wumiguo.ser
 
+import org.slf4j.LoggerFactory
 import org.wumiguo.ser.flow.End2EndFlow
 
 /**
@@ -7,8 +8,12 @@ import org.wumiguo.ser.flow.End2EndFlow
  *         Created on 2020/6/18
  *         (Change file header on Settings -> Editor -> File and Code Templates)
  */
-object ERFlowLauncher{
+object ERFlowLauncher {
+  val log = LoggerFactory.getLogger(getClass.getName)
+
   def main(args: Array[String]): Unit = {
-     End2EndFlow.run
+    log.info("start spark-er flow now")
+    End2EndFlow.run
+    log.info("end spark-er flow now")
   }
 }
