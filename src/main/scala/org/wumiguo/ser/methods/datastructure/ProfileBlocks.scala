10505336123f2fdc55dd8bb22c50370f246aa2aa
levinliu <levinmhliu@gmail.com>
2020-07-13 17:11:06 +0800

    add comment on the class

--- src/main/scala/org/wumiguo/ser/methods/datastructure/ProfileBlocks.scala
+++ src/main/scala/org/wumiguo/ser/methods/datastructure/ProfileBlocks.scala
@@ -1,9 +1,18 @@
 package org.wumiguo.ser.methods.datastructure
 
 /**
-  * Maps a profile with the blocks in which it is contained.
-  * For each block is mantained its size.
-  * @author Luca Gagliardelli
-  * @since 2016/12/08
-  */
-case class ProfileBlocks(profileID : Int, blocks : Set[BlockWithComparisonSize]) extends Serializable{}
+ * ProfileBlocks : represent a profile with underlying blocks with comparisionSize.
+ * E.g. a Profile with clean blocks
+ * (pId=1,
+ *  blocks=Set(
+ *   BlockClean(1, Array[Set[Int]](Set[Int](3, 2),Set[Int](2, 5)), 0.9, 1, "tech_222")
+ *   BlockClean(2, Array[Set[Int]](Set[Int](3, 1),Set[Int](1)), 0.9, 1, "bigdata_222")
+ * )
+ * and its profileBLocks should be:
+ *  ProfileBlocks(1,Set(BlockWithComparisonSize(1,4.0),BlockWithComparisonSize(2,2.0)))
+ *
+ * @author levinliu
+ * @param profileID
+ * @param blocks
+ */
+case class ProfileBlocks(profileID: Int, blocks: Set[BlockWithComparisonSize]) extends Serializable {}
